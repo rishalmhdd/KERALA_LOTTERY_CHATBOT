1e377648d1f590fad2c35c5821e1043440517146
interview <interview@srvinfotech.com>
2025-11-07 10:49:47 +0530

    "Update MongoDB connection to use env MONGO_URI"

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
+
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/lotterybot");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.log("❌ DB Connection Error", err);
